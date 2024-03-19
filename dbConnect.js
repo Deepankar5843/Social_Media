3f602ff150b7b1f636494607cfd5cc62e4aaaa75
Deepankar5843 <113820595+Deepankar5843@users.noreply.github.com>
2024-03-19 13:57:48 +0530

    Add files via upload
    Till here i have Done a full Backend process and frontend with authentication page only in backend i have to add deleteProfile route

---
+++ dbConnect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+  const mongoUri = "mongodb://localhost:27017/";
+  try {
+    const connect = await mongoose.connect(mongoUri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log(`MongoDB connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
